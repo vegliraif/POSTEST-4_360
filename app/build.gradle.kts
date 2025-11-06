33c449651b8d1e72fdc99e0267fb03cb93062b7f
vegliraif <veglirafi@gmail.com>
2025-11-06 18:16:24 +0700

    Update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.example.post4_360"
-    compileSdk = 36  // ✅ ubah ke 35 karena kamu belum punya API 36
+    compileSdk = 36 
 
     defaultConfig {
         applicationId = "com.example.post4_360"
         minSdk = 24
-        targetSdk = 36  // ✅ juga ubah ke 35
+        targetSdk = 36  
         versionCode = 1
         versionName = "1.0"
 
@@ -39,23 +39,19 @@ android {
 }
 
 dependencies {
-    // Library dasar Android
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
 
-    // Room Database (penting)
     val room_version = "2.6.1"
     implementation("androidx.room:room-runtime:$room_version")
     kapt("androidx.room:room-compiler:$room_version")
     implementation("androidx.room:room-ktx:$room_version")
 
-    // Coroutine (untuk akses DB di background)
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 
-    // Testing
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
