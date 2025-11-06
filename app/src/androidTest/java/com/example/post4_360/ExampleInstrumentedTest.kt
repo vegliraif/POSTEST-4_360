614b6c94a463b6c02ebb1fbe854783749aa0a25e
vegliraif <veglirafi@gmail.com>
2025-11-06 18:12:05 +0700

    Update

--- app/src/androidTest/java/com/example/post4_360/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/post4_360/ExampleInstrumentedTest.kt
@@ -8,17 +8,11 @@ import org.junit.runner.RunWith
 
 import org.junit.Assert.*
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
     @Test
     fun useAppContext() {
-        // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.post4_403", appContext.packageName)
+        assertEquals("com.example.post4_360", appContext.packageName)
     }
 }
